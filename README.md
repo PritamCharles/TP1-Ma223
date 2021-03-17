615aa9a5c52f38e172b3b18355b0f0c5a2c6d8a0
PritamCharles <charles.kantane@outlook.fr>
2021-03-17 21:39:22 +0100

    TP fini

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TP1-Ma223
+# Méthode de Gauss pour la résolution de systèmes linéaires
+
+# Personnes ayant participé au projet : 
+# 	- Kantane Pritam Charles, 2PF2
+# 	- Bussière Edgar, 2PF2
---
+++ applications.py
@@ -0,0 +1,58 @@
+# -----------------------------------------------------------------------------
+# Name:        TP1 - Génie Mathématiques
+# Purpose:     applications des fonctions
+#
+# Authors:     KANTANE Pritam Charles
+# Class:       2PF2
+#
+# Created:     23/01/2021
+# Copyright:
+# Licence:     <your licence>
+# -----------------------------------------------------------------------------
+import numpy as np
+import tp01_vf as tp01
+
+#################################################################
+# ### Partie 1 ####
+#################################################################
+
+# Question 1
+print(tp01.ReductionGauss(np.array([[2, 5, 6, 7], [4, 11, 9, 12], [-2, -8, 7, 3]])), "\n")
+
+# Question 2
+print(tp01.ResolutionSysTriSup(tp01.ReductionGauss(np.array([[2, 5, 6, 7], [4, 11, 9, 12], [-2, -8, 7, 3]]))), "\n")
+
+# Question 3
+A = np.array([[2, 5, 6], [4, 11, 9], [-2, -8, 7]])
+B = np.array([[7], [12], [3]])
+print(tp01.Gauss(A, B), "\n")
+
+#################################################################
+# ### Partie 2 ####
+#################################################################
+
+# Question 1
+print(tp01.DecompositionLU(np.array([[2, 5, 6], [4, 11, 9], [-2, -8, 7]])), "\n")
+
+# Question 2
+A = np.array([[2, 5], [4, 9], [-2, -8]])
+L = tp01.DecompositionLU(A)[0]
+U = tp01.DecompositionLU(A)[1]
+B = np.array([[7], [12], [3]])
+print(tp01.ResolutionLU(L, U, B), "\n")
+
+
+
+#################################################################
+# ### Partie 3 ####
+#################################################################
+
+# Question 1
+A = np.array([[2, 5, 6], [4, 11, 9], [-2, -8, 7]])
+B = np.array([[7], [12], [3]])
+print(tp01.GaussChoixPivotPartiel(A, B), "\n")
+
+# Question 2
+A = np.array([[2, 5, 6], [4, 11, 9], [-2, -8, 7]])
+B = np.array([[7], [12], [3]])
+print(tp01.GaussChoixPivotTotal(A, B))
